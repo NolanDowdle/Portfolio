af5e7a6d3995a2d6c8c740537ed550463fb88a9b
Full Name <njdowdle@gmail.com>
2023-06-22 01:50:38 +0700

    added about me section

--- src/App.js
+++ src/App.js
@@ -10,7 +10,25 @@ const App = () => {
       </section>
       <section id="about">
         <h2>About Me</h2>
-        <p>...</p>
+          <div className="educationAndJobs">
+            <div id="educationColumn">
+            <h3>Education</h3>
+              <ul>
+                <li><b>M.S. in Computer Science</b> <br/>North Carolina State University<br/>2021-2023</li>
+                <li><b>B.S. in Computer Science</b> <br/>Appalachian State University<br/>2017-2021</li>
+              </ul>
+            </div>
+            <div id="experienceColumn">
+              <h3>Experience</h3>
+                <ul>
+                  <li><b>Software Engineer Intern</b><br/>Inmar Intelligence<br/>2021-2021</li>
+                </ul>
+            </div>
+          </div>
+        <p>Nice to meet you!<br/> I'm Nolan Dowdle, a Software Engineer who spends most of his time developing groundbreaking AI technologies and has formed a keen interest in NLP.
+        <br/>I also do web development using React, Next.js, Node.js, Firebase, JavaScript, HTML, and CSS.
+        <br/>Throughout my development experience I have used Java, C++, Python, C#, Haskell, and Swift.<br/>
+        <br/>I invite you to take a look at my exciting projects below!</p>
       </section>
       <section id="projects">
         <h2>Projects</h2>
@@ -19,7 +37,13 @@ const App = () => {
       <section id="contact">
         <h2>Contact Me</h2>
         <p>...</p>
-      </section>
+        <div id="socialLinks">
+          <form action="https://github.com/NolanDowdle">
+            <button style={{fontSize: 20}}>GitHub<i className="fa fa-github"></i></button>
+          </form>
+          
+        </div>
+        </section>
     </div>
   );
 };
--- src/navbar.js
+++ src/navbar.js
@@ -4,12 +4,15 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <ul className="navbar-menu">
-          <li className="navbar-item"><a href="#home">Home</a></li>
-          <li className="navbar-item"><a href="#about">About</a></li>
-          <li className="navbar-item"><a href="#projects">Projects</a></li>
-          <li className="navbar-item"><a href="#contact">Contact</a></li>
-        </ul>
+        <>
+        <img src={require('./unnamed.jpg')} />
+          <ul className="navbar-menu">
+            <li className="navbar-item"><a href="#home">Home</a></li>
+            <li className="navbar-item"><a href="#about">About</a></li>
+            <li className="navbar-item"><a href="#projects">Projects</a></li>
+            <li className="navbar-item"><a href="#contact">Contact</a></li>
+          </ul>
+        </>
       </div>
     </nav>
   );
